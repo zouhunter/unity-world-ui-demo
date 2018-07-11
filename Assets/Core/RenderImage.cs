9f2ffa868e7c9dfa3d9065cc5bd8be8373f5c8f6
zouhangte <zouhantezbm@126.com>
2018-07-11 10:13:50 +0800

    添加button,toggle,Slider的演示

---
+++ Assets/Core/RenderImage.cs
@@ -0,0 +1,73 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Events;
+using System.Collections;
+using System.Collections.Generic;
+
+public class RenderImage : Graphic
+{
+    [SerializeField]
+    private Renderer m_renderer;
+    private Material _mat;
+
+    public override Material material
+    {
+        get
+        {
+            if (m_renderer == null)
+            {
+                m_renderer = GetComponentInChildren<Renderer>();
+            }
+
+            if (m_renderer != null && _mat == null)
+            {
+                _mat = new Material(m_renderer.sharedMaterial);
+                m_renderer.material = _mat;
+                Debug.Log("new material");
+            }
+            return _mat;
+        }
+
+        set
+        {
+            _mat = value;
+        }
+    }
+    public override void CrossFadeColor(Color targetColor, float duration, bool ignoreTimeScale, bool useAlpha)
+    {
+        if (material)
+            material.color = targetColor;
+    }
+    public override void CrossFadeAlpha(float alpha, float duration, bool ignoreTimeScale)
+    {
+        if (material)
+        {
+            var color = material.color;
+            color.a = alpha;
+            material.color = color;
+        }
+    }
+    public override Color color
+    {
+        get
+        {
+            if (material)
+                return material.color;
+            return Color.clear;
+        }
+
+        set
+        {
+            Debug.Log(value);
+            if (material)
+                material.color = value;
+        }
+    }
+    public override Material defaultMaterial
+    {
+        get
+        {
+            return m_Material;
+        }
+    }
+}
---
+++ Assets/Demo/Plantform.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Events;
+using System.Collections;
+using System.Collections.Generic;
+using System;
+
+public class Plantform : MonoBehaviour {
+    [SerializeField]
+    private Button m_btn;
+    [SerializeField]
+    private Slider m_slider;
+    [SerializeField]
+    private Toggle m_tog;
+
+    private void Awake()
+    {
+        m_btn.onClick.AddListener(OnBtnClicked);
+        m_slider.onValueChanged.AddListener(OnSliderChanged);
+        m_tog.onValueChanged.AddListener(OnToggleChanged);
+    }
+
+    private void OnSliderChanged(float arg0)
+    {
+        Debug.Log("OnSliderChanged:" + arg0);
+    }
+
+    private void OnToggleChanged(bool arg0)
+    {
+        Debug.Log("OnToggleChanged:" + arg0);
+    }
+
+    private void OnBtnClicked()
+    {
+        Debug.Log("OnAClicked");
+    }
+}
